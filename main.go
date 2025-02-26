79aab27bfec28644dd80bbe39447f8296ee88c4d
Alex Coomans <alex.coomans@withpersona.com>
2025-02-25 23:01:35 -0600

    Upgrade repo to more closely match template (#39)

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright (c) HashiCorp, Inc.
+// Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
 
 package main
@@ -9,19 +9,8 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-
-	"terraform-provider-stablepairer/internal/provider"
+	"github.com/persona-id/terraform-provider-stablepairer/internal/provider"
 )
-
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
-
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
-// ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ./examples/
-
-// Run the docs generation tool, check its repository for more information on how it works and how docs
-// can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 var (
 	// these will be set by the goreleaser configuration
